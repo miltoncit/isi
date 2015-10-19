b1488da1943aeff4c2b29c6e8925acb53b43821d
Milton Gonçalves <milton@ciandt.com>
2015-10-19 17:22:10 -0200

    Initial Commit

---
+++ templates/isi.tpl.php
@@ -0,0 +1,14 @@
+<div id="prod_review_control" class="productReviewWrappers pane-conseils-pour-aller-plus-loin">
+  <h2 class="pane-title expand-details product-reviews"><?php print t('Product Reviews'); ?></h2>
+    <div class="relatedAndReviewHeadWrapper">
+      <div class="bvwrapper">
+        <ul class="BV_button_wrapper">
+          <li><a id="ntgca_readreview" class="ntgca_readreview activentgbutton" href="javascript:void(0);"><?php print t('Read Reviews'); ?></a></li>
+          <li><a id="ntgca_writereview" class="ntgca_writereview" href="javascript:void(0);"><?php print t('Write Reviews'); ?></a></li>
+        </ul>
+        <div id="BVRRContainer_wrapper"><div id="BVRRContainer"><?php print $seo_content; ?></div></div>			
+        <div id="BVSubmissionContainer_wrapper"><div id="BVSubmissionContainer"></div></div>
+      </div>
+    </div>
+  <div class="reviewTextArea"></div>
+</div>
